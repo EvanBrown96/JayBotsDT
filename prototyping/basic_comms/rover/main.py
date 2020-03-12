8fd4e55a7158e17ab280b5bdedeba295420e1ccc
unknown <evanbrown@ku.edu>
2020-03-12 18:26:14 -0500

    updated embedded code to work with winforms app

--- prototyping/basic_comms/rover/main.py
+++ prototyping/basic_comms/rover/main.py
@@ -14,6 +14,18 @@ right_fwd = DigitalOutputDevice(5)
 right_bck = DigitalOutputDevice(6)
 right_spd = PWMOutputDevice(13, frequency=500)
 
+spd_mappings = {
+    "m-ss": (0, 0),
+    "m-fs": (1, 1),
+    "m-bs": (-1, -1),
+    "m-sr": (1, -1),
+    "m-fr": (1, 0.5),
+    "m-br": (-1, -0.5),
+    "m-sl": (-1, 1),
+    "m-fl": (0.5, 1),
+    "m-bl": (-0.5, 1)
+}
+
 while True:
     conn, addr = sock.accept()
 
@@ -22,14 +34,22 @@ while True:
         if not data:
             break
         if data != "persist":
-            x = int(float(data.split(',')[0]))
-            y = int(float(data.split(',')[1]))
 
-            left_velocity = (y+x)/2
-            right_velocity = (y-x)/2
+            if data in spd_mappings.keys():
+                left_velocity = spd_mappings[data][0]
+                right_velocity = spd_mappings[data][1]
 
-            left_spd.value = float(abs(left_velocity))/100
-            right_spd.value = float(abs(right_velocity))/100
+                left_spd.value = abs(left_velocity)
+                right_spd.value = abs(right_velocity)
+            else:
+                x = int(float(data.split(',')[0]))
+                y = int(float(data.split(',')[1]))
+
+                left_velocity = (y+x)/2
+                right_velocity = (y-x)/2
+
+                left_spd.value = float(abs(left_velocity))/100
+                right_spd.value = float(abs(right_velocity))/100
 
             if left_velocity > 0:
                 left_fwd.on()
