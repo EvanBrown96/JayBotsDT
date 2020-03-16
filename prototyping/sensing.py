240af3b5248ef1a44f76a0563b4420da48ad8c64
unknown <evanbrown@ku.edu>
2020-03-16 13:07:54 -0500

    fix string error

--- prototyping/sensing.py
+++ prototyping/sensing.py
@@ -7,5 +7,5 @@ test = DistanceSensor(24, 23, max_distance=DIST_MAX)
 test2 = DistanceSensor(22, 27, max_distance=DIST_MAX)
 
 while True:
-    print("1: " + test.distance + ", 2: " + test2.distance)
+    print("1: {}, 2: {}".format(test.distance, test2.distance))
     time.sleep(0.5)
