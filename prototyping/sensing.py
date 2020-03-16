83a85d2ca5bb22123e8c6826e0e3b39c8bc041b7
unknown <evanbrown@ku.edu>
2020-03-16 12:30:20 -0500

    add second sensor code

--- prototyping/sensing.py
+++ prototyping/sensing.py
@@ -1,8 +1,11 @@
 from gpiozero import DistanceSensor
 import time
 
-test = DistanceSensor(24, 23, max_distance=2.0)
+DIST_MAX = 2.0
+
+test = DistanceSensor(24, 23, max_distance=DIST_MAX)
+test2 = DistanceSensor(22, 27, max_distance=DIST_MAX)
 
 while True:
-    print(test.distance)
+    print("1: " + test.distance + ", 2: " + test2.distance)
     time.sleep(0.5)
