aac840a09e3e83f45add2d398a16a13e02269f31
unknown <evanbrown@ku.edu>
2020-03-16 13:39:25 -0500

    add 100*

--- prototyping/sensing.py
+++ prototyping/sensing.py
@@ -7,5 +7,5 @@ test = DistanceSensor(24, 23, max_distance=DIST_MAX)
 test2 = DistanceSensor(22, 27, max_distance=DIST_MAX)
 
 while True:
-    print("1: {}, 2: {}".format(test.distance, test2.distance))
+    print("1: {}, 2: {}".format(100*test.distance, 100*test2.distance))
     time.sleep(0.5)
