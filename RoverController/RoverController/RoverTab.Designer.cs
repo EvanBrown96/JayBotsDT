d7ca507efe02c0882f86d70a7f2f36cade10c4fa
Evan Brown <evanbrown@ku.edu>
2020-03-28 13:27:53 -0500

    changes for distance display

--- RoverController/RoverController/RoverTab.Designer.cs
+++ RoverController/RoverController/RoverTab.Designer.cs
@@ -35,7 +35,9 @@
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -115,12 +117,22 @@
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Location = new System.Drawing.Point(0, 112);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(446, 203);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Control";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Depth:";
             // 
             // RoverTab
             // 
@@ -132,6 +144,8 @@
             this.Size = new System.Drawing.Size(446, 315);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -145,5 +159,6 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- prototyping/basic_comms/rover/main.py
+++ prototyping/basic_comms/rover/main.py
@@ -1,19 +1,30 @@
 import socket
-from gpiozero import DigitalOutputDevice, PWMOutputDevice
+from gpiozero import DigitalOutputDevice, PWMOutputDevice, DistanceSensor
 import sys
+import time
+import threading
 
 PORT = 10001
 sock = socket.socket()
 sock.bind(('', PORT))
 sock.listen()
 
-# left_fwd = DigitalOutputDevice(20)
-# left_bck = DigitalOutputDevice(21)
-# left_spd = PWMOutputDevice(12, frequency=500)
-#
-# right_fwd = DigitalOutputDevice(5)
-# right_bck = DigitalOutputDevice(6)
-# right_spd = PWMOutputDevice(13, frequency=500)
+sock2 = socket.socket()
+sock2.bind(('', PORT+1))
+sock2.listen()
+
+left_fwd = DigitalOutputDevice(20)
+left_bck = DigitalOutputDevice(21)
+left_spd = PWMOutputDevice(12, frequency=500)
+
+right_fwd = DigitalOutputDevice(5)
+right_bck = DigitalOutputDevice(6)
+right_spd = PWMOutputDevice(13, frequency=500)
+
+DIST_MAX = 2.0
+
+test = DistanceSensor(24, 23, max_distance=DIST_MAX)
+test2 = DistanceSensor(22, 27, max_distance=DIST_MAX)
 
 spd_mappings = {
     "m-ss": (0, 0),
@@ -27,56 +38,79 @@ spd_mappings = {
     "m-bl": (-0.5, 1)
 }
 
-while True:
-    try:
-        conn, addr = sock.accept()
+def one():
+    while True:
+        try:
+            conn, addr = sock.accept()
 
-        while True:
-            data = conn.recv(1)
-            print(data)
-            continue
-            if not data:
-                break
-            if data != "persist":
+            while True:
+                data = conn.recv(1024)
 
-                if data in spd_mappings.keys():
-                    left_velocity = spd_mappings[data][0]
-                    right_velocity = spd_mappings[data][1]
+                if not data:
+                    break
+                if data != "persist":
 
-                    left_spd.value = 0.5*abs(left_velocity)
-                    right_spd.value = 0.5*abs(right_velocity)
-                else:
-                    x = int(float(data.split(',')[0]))
-                    y = int(float(data.split(',')[1]))
+                    if data in spd_mappings.keys():
+                        left_velocity = spd_mappings[data][0]
+                        right_velocity = spd_mappings[data][1]
 
-                    left_velocity = (y+x)/2
-                    right_velocity = (y-x)/2
+                        left_spd.value = 0.5*abs(left_velocity)
+                        right_spd.value = 0.5*abs(right_velocity)
+                    else:
+                        x = int(float(data.split(',')[0]))
+                        y = int(float(data.split(',')[1]))
 
-                    left_spd.value = float(abs(left_velocity))/100
-                    right_spd.value = float(abs(right_velocity))/100
+                        left_velocity = (y+x)/2
+                        right_velocity = (y-x)/2
 
-                if left_velocity > 0:
-                    left_fwd.on()
-                    left_bck.off()
-                elif left_velocity < 0:
-                    left_fwd.off()
-                    left_bck.on()
-                else:
-                    left_fwd.off()
-                    left_bck.off()
+                        left_spd.value = float(abs(left_velocity))/100
+                        right_spd.value = float(abs(right_velocity))/100
 
-                if right_velocity > 0:
-                    right_fwd.on()
-                    right_bck.off()
-                elif right_velocity < 0:
-                    right_fwd.off()
-                    right_bck.on()
-                else:
-                    right_fwd.off()
-                    right_bck.off()
+                    if left_velocity > 0:
+                        left_fwd.on()
+                        left_bck.off()
+                    elif left_velocity < 0:
+                        left_fwd.off()
+                        left_bck.on()
+                    else:
+                        left_fwd.off()
+                        left_bck.off()
 
-    except KeyboardInterrupt:
-        conn.close()
-        sys.exit(0);
-    except:
-        pass
+                    if right_velocity > 0:
+                        right_fwd.on()
+                        right_bck.off()
+                    elif right_velocity < 0:
+                        right_fwd.off()
+                        right_bck.on()
+                    else:
+                        right_fwd.off()
+                        right_bck.off()
+
+        except KeyboardInterrupt:
+            conn.close()
+            sys.exit(0)
+        except:
+            pass
+
+def two():
+    last_time = 0
+
+    while True:
+        try:
+            conn, addr = sock2.accept()
+
+            while True:
+                if time.time() < last_time or time.time() > last_time + 0.5:
+                    msg = "L {},R {}".format(100 * test.distance, 100 * test2.distance)
+                    #msg = "L {},R {}".format(100, 101)
+                    conn.send(msg.encode('utf-8'))
+                    last_time = time.time()
+        except KeyboardInterrupt:
+            conn.close()
+            sys.exit(0)
+        except:
+            pass
+
+threading.Thread(None, one).start()
+threading.Thread(None, two).start()
+
