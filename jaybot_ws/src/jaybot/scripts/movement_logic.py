20fc583b6f57e3bd5a7a59c1e223c50e5c4fbf4d
Evan Brown <eab759@gmail.com>
2020-04-07 02:50:02 -0500

    update movement logic node to stop if it senses an obstacle during manual control

--- jaybot_ws/src/jaybot/scripts/movement_logic.py
+++ jaybot_ws/src/jaybot/scripts/movement_logic.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
+from threading import RLock
+
 
 class Mode:
     MANUAL = 0
 
 
 mode = Mode.MANUAL
+movement_state = 'stop'
 vel_cmd_pub = None
+
+left_avoiding = False
+right_avoiding = False
+
+callback_lock = RLock()
 
 
 def commandCallback(user_command):
@@ -19,15 +27,54 @@ def commandCallback(user_command):
     if cmd[0] == 'm':
         mode = Mode.MANUAL
         if cmd[2] == 'f':
-            vel_cmd_pub.publish('forward')
+            movement_state = 'forward'
         elif cmd[2] == 'b':
-            vel_cmd_pub.publish('backward')
+            movement_state = 'backward'
         elif cmd[3] == 'r':
-            vel_cmd_pub.publish('right')
+            movement_state = 'right'
         elif cmd[3] == 'l':
-            vel_cmd_pub.publish('left')
+            movement_state = 'left'
         else:
+            movement_state = 'stop'
+        
+        callback_lock.acquire()
+        if movement_state == 'forward' and (left_avoiding or right_avoiding):
             vel_cmd_pub.publish('stop')
+        else:
+            vel_cmd_pub.publish(movement_state)
+        callback_lock.release()
+
+
+def leftAvoidance(left_status):
+    global mode, left_avoiding
+    callback_lock.acquire()
+
+    left_avoiding = left_status.data
+
+    if mode == Mode.MANUAL:
+        if not right_avoiding:
+            if left_status.data:
+                vel_cmd_pub.publish('stop')
+            elif movement_state == 'forward':
+                vel_cmd_pub.publish(movement_state)
+
+    callback_lock.release()
+
+
+def rightAvoidance(right_status):
+    global mode, right_avoiding
+    callback_lock.acquire()
+
+    right_avoiding = right_status.data
+
+    if mode == Mode.MANUAL:
+        if not left_avoiding:
+            if right_status.data:
+                vel_cmd_pub.publish('stop')
+            elif movement_state == 'forward':
+                vel_cmd_pub.publish(movement_state)
+
+    callback_lock.release()
 
 
 def setup_node():
@@ -36,10 +83,13 @@ def setup_node():
     rospy.init_node('movement_logic')
 
     rospy.Subscriber('/jayrover/user_cmd', String, commandCallback)
+    rospy.Subscriber('/jayrover/sonar/left_threshold', Bool, leftAvoidance)
+    rospy.Subscriber('/jayrover/sonar/right_threshold', Bool, rightAvoidance)
 
-    vel_cmd_pub = rospy.Publisher('/jayrover/vel_cmd', String, queue_size=10)
+    vel_cmd_pub = rospy.Publisher('/jayrover/dvel_cmd', String, queue_size=10)
 
     rospy.spin()
+
 
 if __name__ == '__main__':
     setup_node()
