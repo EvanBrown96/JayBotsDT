dbd98540e19fe3d67e97c588be0f49bc908c4af9
Evan Brown <evanbrown@ku.edu>
2020-04-13 17:09:28 -0500

    update launch file

--- setup_remote.bash
+++ setup_remote.bash
@@ -1,3 +1,5 @@
 source setup.bash
 #echo "source $HOME/JayBotsDT/remote_ws/devel/setup.bash" >> ~/.bashrc
 sudo apt -y install ros-melodic-rosbridge-suite ros-melodic-hector-slam
+echo "export ROS_IP=\$(ip addr show wifi0 | grep -Po 'inet \K[\d.]+')" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://\$ROS_IP:11311" >> ~/.bashrc
