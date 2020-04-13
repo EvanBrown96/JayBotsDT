574579d461fbb1024feb82206ca7e978b62d5e30
Evan Brown <evanbrown@ku.edu>
2020-04-13 00:56:29 -0500

    improve run.bat

--- RoverController/run.bat
+++ RoverController/run.bat
@@ -1,2 +1,2 @@
+start %~dp0/RoverController/bin/Debug/RoverController.exe
 wsl source /opt/ros/melodic/setup.bash ; export ROS_HOSTNAME=$(hostname) ; export ROS_MASTER_URI=http://$(hostname):11311 ; echo $ROS_MASTER_URI ; roslaunch rosbridge_server rosbridge_websocket.launch
-RoverController/bin/Debug/RoverController.exe
