38466d37e71183c9c5e0b954d1f67ecc54ee4644
Evan Brown <evanbrown@ku.edu>
2020-04-12 23:17:42 -0500

    improve setup files further

---
+++ setup_remote.bash
@@ -0,0 +1,3 @@
+source setup.bash
+echo "source $HOME/JayBotsDT/remote_ws/devel/setup.bash" >> ~/.bashrc
+sudo apt -y install ros-melodic-rosbridge-suite
---
+++ setup_rpi.bash
@@ -0,0 +1,12 @@
+source setup.bash
+# sudo sh -c 'echo "deb https://packages.ubiquityrobotics.com/ubuntu/ubiquity xenial main" > /etc/apt/sources.list.d/ubiquity-latest.list'
+# sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C3032ED8
+sudo dpkg -i --force-all /var/cache/apt/archives/linux-firmware-raspi2_1.20190819-0ubuntu0.18.04.1_armhf.deb
+sudo apt -y full-upgrade
+sudo apt -y install xrdp openssh-server
+echo "source $HOME/JayBotsDT/jaybot_ws/devel/setup.bash" >> ~/.bashrc
+echo "sudo chmod 666 /dev/ttyUSB0" >> ~/.bashrc
+pip install gpiozero pigpio
+sudo systemctl enable ssh.service
+sudo systemctl start ssh.service
+sudo dpkg-reconfigure openssh-server
