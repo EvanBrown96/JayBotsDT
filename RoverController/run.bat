21b94bc0def2956ea1209726cea0462339f5ac61
Evan Brown <evanbrown@ku.edu>
2020-04-13 02:12:39 -0500

    improve run.bat, add rplidarNode to launch file

--- RoverController/run.bat
+++ RoverController/run.bat
@@ -1,2 +1,2 @@
 start %~dp0/RoverController/bin/Debug/RoverController.exe
-wsl source /opt/ros/melodic/setup.bash ; export ROS_HOSTNAME=$(hostname) ; export ROS_MASTER_URI=http://$(hostname):11311 ; echo $ROS_MASTER_URI ; roslaunch rosbridge_server rosbridge_websocket.launch
+wsl source /opt/ros/melodic/setup.bash ; export ROS_HOSTNAME=$(ip addr show wifi0 ^| grep -Po 'inet \K[\d.]+') ; export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311 ; echo $ROS_MASTER_URI ; roslaunch rosbridge_server rosbridge_websocket.launch
