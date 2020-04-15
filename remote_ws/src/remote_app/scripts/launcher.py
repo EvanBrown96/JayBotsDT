2626067785cc3a4642451a04b0aeebf0ffbe1960
Evan Brown <evanbrown@ku.edu>
2020-04-14 20:29:53 -0500

    improve launcher node & launch file

--- remote_ws/src/remote_app/scripts/launcher.py
+++ remote_ws/src/remote_app/scripts/launcher.py
@@ -1,40 +1,53 @@
+#!/usr/bin/env python
+
 import subprocess
 import rospy
-from std_msgs.msg import String
-
+from remote_app.srv import LaunchRover, LaunchRoverResponse
 
 running = {}
 
 
-def doLaunch(launch_info):
-    machine_name = launch_info.data.split(',')[0]
-    ip_addr = launch_info.data.split(',')[1]
-    proc = subprocess.Popen(
-        ['roslaunch', '~/JayBotsDT/launch/remote_standard.launch',
-        'machine_name:={}'.format(machine_name), 'ip_addr:={}'.format(ip_addr)])
-    running[machine_name] = proc
+def doLaunch(req):
+    rospy.loginfo(req)
+    try:
+        proc = subprocess.Popen(
+            ['roslaunch', '~/JayBotsDT/launch/remote_standard.launch',
+            'machine_name:={}'.format(req.machine_name), 'ip_addr:={}'.format(req.ip_addr)])
+        running[req.machine_name] = proc
+        return LaunchRoverResponse("")
+    except e:
+        return LaunchRoverResponse(str(e))
 
-def killRover(kill_info):
-    machine_name = launch_info.data
-    running[machine_name].terminate()
+
+def killRover(req):
+    try:
+        running[req.machine_name].terminate()
+        return KillRoverResponse(True)
+    except:
+        return KillRoverResponse(False)
+
 
 def setup_node():
 
     rospy.init_node('launcher');
-    rospy.Subscriber('launch_rover', String, doLaunch)
-    rospy.Subscriber('kill_rover', String, killRover)
+    rospy.Service('/launch_rover', LaunchRover, doLaunch)
+    rospy.spin()
 
-    rate = rospy.Rate(1)
 
-    while not rospy.is_shutdown():
-
-        for machine_name in running.keys():
-            status = running[machine_name].poll()
-            if status is not None:
-                print(status)
-                del running[machine_name]
-
-        rate.sleep()
+    # rospy.Subscriber('launch_rover', String, doLaunch)
+    # rospy.Subscriber('kill_rover', String, killRover)
+    #
+    # rate = rospy.Rate(1)
+    #
+    # while not rospy.is_shutdown():
+    #
+    #     for machine_name in running.keys():
+    #         status = running[machine_name].poll()
+    #         if status is not None:
+    #             print(status)
+    #             del running[machine_name]
+    #
+    #     rate.sleep()
 
 if __name__ == '__main__':
     setup_node()
