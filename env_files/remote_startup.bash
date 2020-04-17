71de2bc035bb76b7a33304f6d36f1d653b991fe1
Evan Brown <evanbrown@ku.edu>
2020-04-17 07:06:42 -0500

    fix ip get

--- env_files/remote_startup.bash
+++ env_files/remote_startup.bash
@@ -1,5 +1,5 @@
 source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 ls
-export ROS_IP=$(ip addr show eth1 | grep -Po 'inet \K[\d.]+')
+export ROS_IP=$(ip addr show eth2 | grep -Po 'inet \K[\d.]+')
 export ROS_MASTER_URI=http://$ROS_IP:11311
