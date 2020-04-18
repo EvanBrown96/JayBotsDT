679d9029ce0b4da0b352ff043bb1cdd36152b46d
Evan Brown <evanbrown@ku.edu>
2020-04-17 19:38:46 -0500

    make it so rviz starts along with application

--- RoverController/run.bat
+++ RoverController/run.bat
@@ -12,9 +12,17 @@ set /P DOCKER_ID=<%TEMP_LOC%
 docker exec %DOCKER_ID% /bin/bash -c "ip addr show eth2 | grep -Po 'inet \K[\d.]+'" > %TEMP_LOC%
 set /P WS_ADDR=<%TEMP_LOC%
 
+set ROS_MASTER_URI=http://%WS_ADDR%:11311
+python -c "import socket; hostname=socket.gethostname(); print(socket.gethostbyname_ex(hostname)[-1][-1])" > %TEMP_LOC%
+set /P ROS_IP=<%TEMP_LOC%
 rm %TEMP_LOC%
 
-%~dp0\RoverController\bin\Debug\RoverController.exe
+echo Waiting for ROS to initialize...
+sleep 7
+
+start /MIN cmd /C "c:\opt\ros\melodic\x64\setup.bat && rviz -d %~dp0\jaybot.rviz"
+
+%~dp0\RoverController\bin\Release\RoverController.exe
 
 docker stop %DOCKER_ID%
 
---
+++ RoverController/rviz/run_rviz.bat
@@ -0,0 +1,3 @@
+c:\opt\ros\melodic\x64\setup.bat
+echo %ROS_IP%
+rviz
