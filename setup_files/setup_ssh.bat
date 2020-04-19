68f0d0c59e1d69c83d02955a5b37ea70ed592feb
EvanBrown96 <evanbrown@ku.edu>
2020-04-19 00:27:34 -0500

    replace cat with type for windows

--- setup_files/setup_ssh.bat
+++ setup_files/setup_ssh.bat
@@ -1,2 +1,2 @@
 ssh -oHostKeyAlgorithms=ssh-rsa %1 mkdir -p .ssh
-cat %USERPROFILE%\.ssh\id_rsa.pub | ssh %1 "cat >> ~/.ssh/authorized_keys"
+type %USERPROFILE%\.ssh\id_rsa.pub | ssh %1 "cat >> ~/.ssh/authorized_keys"
