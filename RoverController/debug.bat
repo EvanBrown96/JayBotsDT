2addefe209fc84671bcc2975a76f010e3045fa02
Evan Brown <evanbrown@ku.edu>
2020-04-19 00:04:14 -0500

    update debug file

--- RoverController/debug.bat
+++ RoverController/debug.bat
@@ -10,10 +10,17 @@ set /P DOCKER_ID=<%TEMP_LOC%
 docker exec %DOCKER_ID% /bin/bash -c "ip addr show eth2 | grep -Po 'inet \K[\d.]+'" > %TEMP_LOC%
 set /P WS_ADDR=<%TEMP_LOC%
 
+set ROS_MASTER_URI=http://%WS_ADDR%:11311
+python -c "import socket; hostname=socket.gethostname(); print(socket.gethostbyname_ex(hostname)[-1][-1])" > %TEMP_LOC%
+set /P ROS_IP=<%TEMP_LOC%
 rm %TEMP_LOC%
 
-echo %WS_ADDR%
-start %~dp0\RoverController\bin\Debug\RoverController.exe
+echo ROS_MASTER_URI=%ROS_MASTER_URI%
+echo ROS_IP=%ROS_IP%
+
+start /MIN cmd /C "c:\opt\ros\melodic\x64\setup.bat && rviz -d %~dp0\jaybot.rviz"
+
+start %~dp0\RoverController\bin\Release\RoverController.exe
 
 docker attach %DOCKER_ID%
 
