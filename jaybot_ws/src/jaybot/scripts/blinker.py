853fcca9e48fe91eaec012c8732b73e98feb905f
Evan Brown <evanbrown@ku.edu>
2020-04-22 23:42:10 -0500

    add blinker to launch file

--- jaybot_ws/src/jaybot/scripts/blinker.py
+++ jaybot_ws/src/jaybot/scripts/blinker.py
@@ -4,7 +4,6 @@ from gpiozero import LED
 import rospy
 from std_srvs.srv import Empty, EmptyResponse
 
-#-- RIGHT
 BLINKER_GPIO = 25
 led = None
 
@@ -21,22 +20,14 @@ def start_blinker():
     rospy.init_node('blinker')
     rospy.loginfo("starting node")
 
-    rospy.Service("/start_blinker", Empty, start)
+    rospy.Service("start_blinker", Empty, start)
     rospy.loginfo("started /start_blinker service")
-    rospy.Service("/stop_blinker", Empty, stop)
+    rospy.Service("stop_blinker", Empty, stop)
     rospy.loginfo("started /stop_blinker service")
 
     led = LED(BLINKER_GPIO)
 
     rospy.spin()
-
-    # rate = rospy.Rate(1)
-    #
-    # while not rospy.is_shutdown():
-    #     rate.sleep()
-    #     led.
-    #     if blinking:
-
 
     rospy.loginfo("stopping node")
 
